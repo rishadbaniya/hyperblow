6b6c5884435f9ac6089d7b404f1acb88b3331365
rishadbaniya <rbdroid01@gmail.com>
2021-09-19 08:15:06 +0000

    chore: Prints the name of torrent and total no. of files

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ extern crate serde_bencode;
 #[macro_use]
 extern crate serde_derive;
 
-mod torrent_parser;
+mod torrent_details;
+pub mod torrent_parser;
 
 use std::env;
+use torrent_details::spit_details;
 use torrent_parser::FileMeta;
 
 fn main() {
@@ -17,6 +19,5 @@ fn main() {
     // Get the argument at index 1 from the CLI command "rtourent xyz.torrent"
     // So that we can get the name of the file i.e xyz.torrent
     let torrentParsed: FileMeta = torrent_parser::parse_file(&args[1]);
-
-    println!("{:?}", torrentParsed);
+    spit_details(&torrentParsed);
 }
