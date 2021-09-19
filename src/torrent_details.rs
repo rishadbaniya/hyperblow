cdd689ef55e874cc839f5fa4bcbb2ce2db12443e
rishadbaniya <rbdroid01@gmail.com>
2021-09-19 09:45:34 +0000

    feat: Added a way to count all no. of files and folders that torrent has info about

--- src/torrent_details.rs
+++ src/torrent_details.rs
@@ -8,7 +8,27 @@ pub fn spit_details(fileMeta: &FileMeta) {
     }
 
     if let Some(allFiles) = &fileMeta.info.files {
-        println!("Total Files -> {}", allFiles.len());
+        // In FileMeta.info.files, the vector contains list of all vectors which contains
+        // information of directory and at last file name
+        // Eg. [["a","b","c.txt"],[["a"],["d.txt"]] => It means a/b/c.txt and a/d.txt
+        // So getting every vector within this vector has a file name at its last index.
+        // Getting the length of this parent vector will give us all total no. of files
+        let totalFiles = allFiles.len();
+
+        // Declaring and initializing  an empty vector to store all unique folders name
+        let mut allFolders: Vec<String> = Vec::new();
+
+        //Get all the unique folders from the allFiles Vector
+        for x in allFiles {
+            if x.path.len() > 1 {
+                for z in 0..(x.path.len() - 1) {
+                    if !allFolders.contains(&x.path[z]) {
+                        allFolders.push(x.path[z].clone());
+                    }
+                }
+            }
+        }
+        println!("Total Files -> {}", totalFiles);
+        println!("Total Folders -> {}", allFolders.len());
     }
-    println!("Total Folders -> ")
 }
