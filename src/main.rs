7d32372f98665bf76dddfd59cb45c67480effb4d
rishadbaniya <rbdroid01@gmail.com>
2021-09-20 04:56:59 +0000

    Used encoder in the main function

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate serde_bencode;
 #[macro_use]
 extern crate serde_derive;
 
+mod percent_encoder;
 mod torrent_details;
 pub mod torrent_parser;
 
@@ -14,12 +15,10 @@ use torrent_details::spit_details;
 fn main() {
     // Get all arguments passed in the CLI
     let args: Vec<String> = env::args().collect();
-
     // Get the argument at index 1 from the CLI command "rtourent xyz.torrent"
     // So that we can get the name of the file i.e xyz.torrent
-    let (torrentParsed, info_hash) = torrent_parser::parse_file(&args[1]);
-
+    let (torrentParsed, info_hash_bytes) = torrent_parser::parse_file(&args[1]);
     spit_details(&torrentParsed);
-
-    println!("{:?}", info_hash);
+    let percentEncodedInfoHash = percent_encoder::encode(info_hash_bytes);
+    println!("{}", percentEncodedInfoHash);
 }
