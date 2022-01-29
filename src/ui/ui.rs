0d973a587f3b71cf79d54b4e102df3f6f6c71f9c
rishadbaniya <rbdroid01@gmail.com>
2022-01-29 18:54:30 +0530

    feat: Added a generic type Error

--- src/ui/ui.rs
+++ src/ui/ui.rs
@@ -5,18 +5,22 @@ use crossterm::execute;
 use crossterm::terminal::{
     disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
 };
-use tui::backend::CrosstermBackend;
+use tui::backend::{Backend, CrosstermBackend};
 use tui::terminal::Terminal;
 
-type Error = Box<dyn std::error::Error>;
+use crate::Result;
 
-pub fn draw_ui() -> Result<(), Error> {
+pub fn draw_ui() -> Result<()> {
     // Enabling the raw mode and using alternate screen
     enable_raw_mode()?;
     let mut stdout = stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+
+    // Call to start the app and draw the UI
+
+    start_and_draw(&mut terminal)?;
 
     // Restoring the terminal
     disable_raw_mode()?;
@@ -26,6 +30,19 @@ pub fn draw_ui() -> Result<(), Error> {
         DisableMouseCapture
     )?;
     terminal.show_cursor()?;
+
+    Ok(())
+}
+
+pub fn start_and_draw<B>(terminal: &mut Terminal<B>) -> Result<()>
+where
+    B: Backend,
+{
+    //terminal.draw(|frame| {});
+
+    if cfg!(debug_assertions) {
+        println!("Yo! this is working");
+    }
 
     Ok(())
 }
