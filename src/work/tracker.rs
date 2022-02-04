5d271e1f46e71e0f1cb4fd2781aa8d67e8b2985f
rishadbaniya <rbdroid01@gmail.com>
2022-02-04 11:06:02 +0530

    feat: Added methods related to announce and added comments

--- src/work/tracker.rs
+++ src/work/tracker.rs
@@ -3,6 +3,7 @@
 
 use bytes::{BufMut, BytesMut};
 use reqwest::Url;
+use std::net::SocketAddr;
 
 const TRACKER_ERROR: &str =
     "There is something wrong with the torrent file you provided \n Couldn't parse a tracker URL";
@@ -10,21 +11,21 @@ const TRACKER_ERROR: &str =
 // Struct to hold data for "Announce"
 // and create a "98 byte" buffer to make "Announce Request"
 // Reference : http://www.bittorrent.org/beps/bep_0015.html
-// IPv4 announce requst:
+// IPv4 announce request:
 // Offset  Size    Name    Value
-// 0       64-bit integer  connection_id
-// 8       32-bit integer  action          1 // announce
-// 12      32-bit integer  transaction_id
-// 16      20-byte string  info_hash
-// 36      20-byte string  peer_id
-// 56      64-bit integer  downloaded
-// 64      64-bit integer  left
-// 72      64-bit integer  uploaded
+// 0       64-bit integer  connection_id   The connection id acquired from establishing the connection.
+// 8       32-bit integer  action          Action. in this case, 1 for announce. See : https://www.rasterbar.com/products/libtorrent/udp_tracker_protocol.html#actions
+// 12      32-bit integer  transaction_id  Randomized by client
+// 16      20-byte string  info_hash       The info-hash of the torrent you want announce yourself in.
+// 36      20-byte string  peer_id         Your peer id. (Peer ID Convention : https://www.bittorrent.org/beps/bep_0020.html)
+// 56      64-bit integer  downloaded      The number of byte you've downloaded in this session.
+// 64      64-bit integer  left            The number of bytes you have left to download until you're finished.
+// 72      64-bit integer  uploaded        The number of bytes you have uploaded in this session.
 // 80      32-bit integer  event           0 // 0: none; 1: completed; 2: started; 3: stopped
-// 84      32-bit integer  IP address      0 // default
-// 88      32-bit integer  key
-// 92      32-bit integer  num_want        -1 // default
-// 96      16-bit integer  port
+// 84      32-bit integer  IP address      Your ip address. Set to 0 if you want the tracker to use the sender of this UDP packet.u
+// 88      32-bit integer  key             A unique key that is randomized by the client.
+// 92      32-bit integer  num_want        The maximum number of peers you want in the reply. Use -1 for default.
+// 96      16-bit integer  port            The port you're listening on.
 // 98
 pub struct Announce {
     connection_id: Option<i64>,
@@ -41,27 +42,34 @@ pub struct Announce {
     num_want: Option<i32>,
     port: Option<i16>,
 }
+
 impl Announce {
     // Creates an empty Announce instance
     pub fn empty() -> Self {
+        let peer_id_slice = b"-BOWxxx-yyyyyyyyyyyy";
+        let mut peer_id = [0u8; 20];
+        for (index, value) in peer_id_slice.iter().enumerate() {
+            peer_id[index] = *value;
+        }
         Announce {
             connection_id: None,
             action: Some(1),
             transaction_id: None,
             info_hash: None,
-            peer_id: None,
+            peer_id: Some(peer_id),
             downloaded: None,
             left: None,
             uploaded: None,
-            event: None,
-            ip_address: None,
+            event: Some(0),
+            ip_address: Some(0),
             key: None,
-            num_want: None,
+            num_want: Some(-1),
             port: None,
         }
     }
 
-    // Consumes the Announce and gives you a Buffer of 98 bytes
+    // Consumes the Announce instance and gives you a Buffer of 98 bytes that you
+    // can use to make Announce Request in UDP
     pub fn getBytesMut(&self) -> BytesMut {
         let mut bytes = BytesMut::with_capacity(98);
         bytes.put_i64(self.connection_id.unwrap());
@@ -84,20 +92,32 @@ impl Announce {
         self.connection_id = Some(v);
     }
 
-    pub fn set_action(&mut self, v: i32) {
-        self.action = Some(v);
-    }
-
     pub fn set_transaction_id(&mut self, v: i32) {
-        self.action = Some(v);
+        self.transaction_id = Some(v);
     }
 
     pub fn set_info_hash(&mut self, v: [u8; 20]) {
         self.info_hash = Some(v);
     }
 
-    pub fn set_peer_id(&mut self, v: [u8; 20]) {
-        self.peer_id = Some(v);
+    pub fn set_downloaded(&mut self, v: i64) {
+        self.downloaded = Some(v);
+    }
+
+    pub fn set_uploaded(&mut self, v: i64) {
+        self.uploaded = Some(v);
+    }
+
+    pub fn set_left(&mut self, v: i64) {
+        self.left = Some(v);
+    }
+
+    pub fn set_port(&mut self, v: i16) {
+        self.port = Some(v);
+    }
+
+    pub fn set_key(&mut self, v: i32) {
+        self.key = Some(v);
     }
 }
 
@@ -112,6 +132,7 @@ pub enum TrackerProtocol {
 pub struct Tracker {
     pub url: Url,
     pub protocol: TrackerProtocol,
+    pub socket_adr: Option<SocketAddr>,
 }
 
 impl Tracker {
@@ -124,7 +145,11 @@ impl Tracker {
                 TrackerProtocol::HTTP
             }
         };
-        Tracker { url, protocol }
+        Tracker {
+            url,
+            protocol,
+            socket_adr: None,
+        }
     }
 
     /// Create list of "Tracker" from data in the
