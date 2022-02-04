2c83d2fb9474679d4acebb7de74daec69c2fa94f
rishadbaniya <rbdroid01@gmail.com>
2022-02-04 18:53:18 +0530

    refactor: Refactored code with some clean code

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,20 @@ use std::{
 use ui::files::FilesState;
 use work::start::start as workStart;
 
-pub type Result<T> = std::result::Result<T, Box<dyn Error>>;
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 // Main thread to work on UI rendering
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
 
-    // Global State Of the App that is Shared Across Threads
-    let appState = Arc::new(Mutex::new(FilesState::new()));
+    let app_state = Arc::new(Mutex::new(FilesState::new()));
 
     // Spawn worker thread
-    let appStateWorkingThread = appState.clone();
+    let appStateWorkingThread = app_state.clone();
     let handle = thread::spawn(move || workStart(appStateWorkingThread, &args[0]));
     handle.join().unwrap();
 
     // Draw the UI
-    ui::ui::draw_ui(appState)?;
+    ui::ui::draw_ui(app_state)?;
     Ok(())
 }
