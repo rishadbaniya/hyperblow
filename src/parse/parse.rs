83c34392f9fa3fd04974d0ce2724be7113693247
rishadbaniya <rbdroid01@gmail.com>
2022-02-05 17:36:57 +0530

    chore: Delegated the work of finding socket address to the parsing phase itself

--- src/parse/parse.rs
+++ src/parse/parse.rs
@@ -1,15 +1,30 @@
 use crate::ui::files::FilesState;
 use crate::work::file::{File, FileType};
 use crate::work::torrent_parser;
+use crate::work::tracker::Tracker;
+use std::cell::RefCell;
 use std::sync::{Arc, Mutex};
+use std::time::Instant;
 
-pub fn parsing_thread_main(file_state: Arc<Mutex<FilesState>>, torrent_file_path: String) {
+// Starting point for the parsing thread
+pub fn parsing_thread_main(
+    file_state: Arc<Mutex<FilesState>>,
+    torrent_file_path: String,
+    trackers: Arc<Mutex<Vec<RefCell<Tracker>>>>,
+) {
     let mut file_state_lock = file_state.lock().unwrap();
-
-    // Gets the metadata from the torrent file and info_hash of the torretnt
+    let mut trackers_lock = trackers.lock().unwrap();
+    let t = Instant::now();
+    // Gets the metadata from the torrent file and info_hash of the torrent
     let (file_meta, info_hash) = torrent_parser::parse_file(&torrent_file_path);
-    println!("Parsed torrent file : \"{}\"", &torrent_file_path);
 
+    println!(
+        "Parsed torrent file : \"{}\"             [{:?}]",
+        &torrent_file_path,
+        Instant::now().duration_since(t)
+    );
+
+    let t = Instant::now();
     // Sets the name of the torrent file for the UI
     file_state_lock.name = file_meta.info.name.as_ref().unwrap().clone();
 
@@ -22,11 +37,12 @@ pub fn parsing_thread_main(file_state: Arc<Mutex<FilesState>>, torrent_file_path
         should_download: true,
     }));
 
+    // Creates file tree
     if let Some(x) = file_meta.info.files.as_ref() {
-        // Creates file tree for multi file mode
+        // Multi file mode
         File::createFileTree(file_state_lock.file.clone(), x);
     } else {
-        // Creates file tree for single file mode
+        // Single file mode
         file_state_lock.file.lock().unwrap().inner_files = Some(vec![Arc::new(Mutex::new(File {
             name: file_meta.info.name.as_ref().unwrap().clone(),
             file_type: FileType::REGULAR,
@@ -36,5 +52,25 @@ pub fn parsing_thread_main(file_state: Arc<Mutex<FilesState>>, torrent_file_path
         }))])
     }
 
-    println!("Generated File Tree");
+    println!(
+        "Generated File Tree                               [{:?}]",
+        Instant::now().duration_since(t)
+    );
+
+    let t = Instant::now();
+    println!("Getting all the trackers socket address........");
+
+    // Gets the socket address of all the Trackers
+    let announce_list: &Vec<Vec<String>> = file_meta.announce_list.as_ref().unwrap();
+    *trackers_lock = Tracker::getTrackers(&file_meta.announce, announce_list);
+    for tracker in &(*trackers_lock) {
+        let mut tracker_borrow_mut = tracker.borrow_mut();
+        if let Ok(addrs) = tracker_borrow_mut.url.socket_addrs(|| None) {
+            tracker_borrow_mut.socket_adr = Some(addrs[0]);
+        }
+    }
+    println!(
+        "Got all the socket address                      [{:?}]",
+        Instant::now().duration_since(t)
+    );
 }
