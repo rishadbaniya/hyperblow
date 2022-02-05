38a718fceda8c5d05fe46ff4fdb450597d8c685c
rishadbaniya <rbdroid01@gmail.com>
2022-02-05 17:44:32 +0530

    Used parser in main

--- src/work/start.rs
+++ src/work/start.rs
@@ -1,81 +1,72 @@
 // NOTE: When i say files, i mean it in Unix term. Folders are files as well, their type is Directory
 
-use super::tracker::{annnounce_request, connect_request};
-use super::tracker::{ConnectResponse, Tracker};
+use super::tracker::Tracker;
 use super::{file, torrent_parser};
-use crate::{
-    ui::files::FilesState,
-    work::{
-        file::File,
-        tracker::{AnnounceRequest, AnnounceResponse, ConnectRequest, TrackerProtocol},
-    },
-};
+use crate::{ui::files::FilesState, work::file::File};
+use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
-use std::{net::SocketAddr, time::Duration};
-use tokio::{net::UdpSocket, time::timeout};
+use std::time::Instant;
+use tokio::net::UdpSocket;
 
 // Starting Point for the working thread
-pub fn start(fileState: Arc<Mutex<FilesState>>, torrent_file_path: &String) {
-    let (torrentParsed, info_hash) = torrent_parser::parse_file(&torrent_file_path);
-    fileState.lock().unwrap().name = torrentParsed.info.name.unwrap();
-    // Root file to store all the files
-    fileState.lock().unwrap().file = Arc::new(Mutex::new(file::File {
-        name: String::from("/"),
-        file_type: file::FileType::DIRECTORY,
-        inner_files: Some(Vec::new()),
-        size: 0,
-        should_download: true,
-    }));
-
-    // Total Torrent Size in Bytes
-    let mut totalSize: i64 = 20000;
-
-    File::createFileTree(
-        fileState.lock().unwrap().file.clone(),
-        torrentParsed.info.files.as_ref().unwrap(),
-    );
-    let i = fileState.lock().unwrap().file.lock().unwrap().size();
-    println!("{:?} ", i);
-
-    const TRANS_ID: i32 = 10;
-    const PORT: i16 = 8001;
-
-    let announce_list: &Vec<Vec<String>> = torrentParsed.announce_list.as_ref().unwrap();
-    let trackers: Vec<Tracker> = Tracker::getTrackers(&torrentParsed.announce, announce_list);
+pub fn start((app_state, torrent_file_path): (Arc<Mutex<FilesState>>, String)) {
+    let (file_meta, info_hash) = torrent_parser::parse_file(&torrent_file_path);
 
     let async_block = async move {
-        // Communicating Socket Address and UDP Socket
-        let socket_address: SocketAddr = format!("[::]:{}", PORT).parse().unwrap();
-        let socket = UdpSocket::bind(socket_address).await.unwrap();
-
-        // Iterate over all the trackers
-        for tracker in trackers {
-            if tracker.protocol == TrackerProtocol::UDP {
-                // Create Connection Request
-                if let Ok(addrs) = tracker.url.socket_addrs(|| None) {
-                    if let Ok(connect_response) =
-                        connect_request(TRANS_ID, &socket, &addrs[0]).await
-                    {
-                        if let Ok(announce_response) = annnounce_request(
-                            connect_response.clone(),
-                            &socket,
-                            &addrs[0],
-                            info_hash.clone(),
-                        )
-                        .await
-                        {
-                            println!(
-                                " Connection Response {:?} | Announce Response : {:?}",
-                                connect_response, announce_response
-                            );
-                        }
-                    }
-                }
-            }
-        }
+        //        tracker_request(info_hash, trackers).await;
+        //for tracker in &trackers {
+        //   println!("{:?}", &tracker);
+        //}
     };
 
     tokio::runtime::Runtime::new()
         .unwrap()
         .block_on(async_block);
+}
+
+// GOAL : Constantly polls the Tracker with UDP request after certain interval
+// CURRENTLY : Blocks the thread
+// TODO : Make this process of polling trackers non blocking, for now it's blocking for idk what
+// reason
+async fn tracker_request(info_hash: Vec<u8>, trackers: Vec<std::cell::RefCell<Tracker>>) {
+    //
+    const TRANS_ID: i32 = 10;
+    const PORT: i16 = 8001;
+    let socket_address: SocketAddr = format!("[::]:{}", PORT).parse().unwrap();
+    let socket = UdpSocket::bind(socket_address).await.unwrap();
+    let x = Instant::now();
+
+    println!("{:?}", Instant::now().duration_since(x));
+
+    //  for tracker in &trackers {
+    //      let tracker_borrow = tracker.borrow();
+    //      let x = std::time::Instant::now();
+    //      if tracker_borrow.protocol == TrackerProtocol::UDP {
+    //          // Create Connection Request
+    //          if let Ok(addrs) = tracker_borrow.url.socket_addrs(|| None) {
+    //              // Sets the socket address of the URL
+    //              let mut tracker_borrow_mut = tracker.borrow_mut();
+    //              tracker_borrow_mut.socket_adr = Some(addrs[0]);
+
+    //              drop(tracker_borrow);
+    //              match connect_request(TRANS_ID, &socket, &addrs[0], tracker).await {
+    //                  Ok(connect_response) => {
+    //                      if let Ok(_) = annnounce_request(
+    //                          connect_response.clone(),
+    //                          &socket,
+    //                          &addrs[0],
+    //                          info_hash.clone(),
+    //                      )
+    //                      .await
+    //                      {
+    //                          tracker.borrow_mut().didItResolve = true;
+    //                      }
+    //                  }
+    //                  _ => {
+    //                      println!("Error Time : {:?}", Instant::now().duration_since(x));
+    //                  }
+    //              }
+    //          }
+    //      }
+    //  }
 }
