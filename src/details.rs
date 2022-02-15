29e39b318896ba49efe031c78c4e30fcdec8e661
rishadbaniya <rbdroid01@gmail.com>
2022-02-15 22:04:57 +0530

    refactor: Replaced RwLock with Mutex

--- src/details.rs
+++ src/details.rs
@@ -1,14 +1,19 @@
 use std::collections::HashSet;
 
-// Struct that holds the state for Details Section of the UI
+/// Struct that holds the state and details of the torrent that is being
+/// downloaded
 #[derive(Debug, Clone)]
 pub struct Details {
+    /// Name of the torrent
     pub name: Option<String>,
+    /// Info hash of the torrent
     pub info_hash: Option<Vec<u8>>,
+    /// Total no of pieces
     pub total_pieces: Option<u32>,
     pub piece_length: Option<i64>,
     pub total_bytes: Option<i64>,
     pub downloaded_bytes: Option<u64>,
+    /// Hash of all the pieces in same index as piece index
     pub pieces_hash: Vec<[u8; 20]>,
     /// Zero based index of the pieces downloaded
     pub pieces_downloaded: HashSet<u32>,
@@ -19,8 +24,6 @@ pub struct Details {
 impl Default for Details {
     fn default() -> Self {
         let pieces_hash = Vec::new();
-        let pieces_downloaded = HashSet::new();
-        let pieces_requested = HashSet::new();
         Self {
             name: None,
             info_hash: None,
@@ -29,8 +32,8 @@ impl Default for Details {
             piece_length: None,
             total_pieces: None,
             pieces_hash,
-            pieces_downloaded,
-            pieces_requested,
+            pieces_downloaded: HashSet::new(),
+            pieces_requested: HashSet::new(),
         }
     }
 }
